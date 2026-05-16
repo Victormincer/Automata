ca15680d10997e7318224ac43eb0ec360d4c6766
Victor Pardo <victorap.punk@gmail.com>
2026-05-16 16:25:11 -0500

    Se crean clases para desarrollar los automatas

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/*
+ * Proposito: Clase principal que permite ejecutar el programa.
+ * 
+ * Autor: Victor Alfonso Pardo Guiterrez - Maryury Hernandez Marin
+ * Fecha: 2024-5-16
+ * versión: 1.0
+ */
+public class Main {
+
+}
---
+++ src/automata/ConversorAFND.java
@@ -0,0 +1,13 @@
+/*
+ * Proposito: Clase que permite convertir un autómata finito no determinista (AFND) a un autómata finito determinista (AFD).
+ * Esta clase puede ser utilizada para realizar la conversión de un AFND a un AFD.
+ * 
+ * Autor: Victor Alfonso Pardo Guiterrez - Maryury Hernandez Marin
+ * Fecha: 2024-5-16
+ * versión: 1.0
+ */
+public class ConversorAFND {
+
+
+
+}
